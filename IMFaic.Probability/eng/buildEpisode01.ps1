3a7c659edae738bfc01e1471ab02dbb0daf1a9ba
Francis de la Cerna <imfrancisd@users.noreply.github.com>
2019-03-11 15:40:24 -0500

    Modify Episode01 build.
    Split Probability into its own file (Probability.dll).

--- IMFaic.Probability/eng/buildEpisode01.ps1
+++ IMFaic.Probability/eng/buildEpisode01.ps1
@@ -12,28 +12,48 @@ $tools = & "$PSScriptRoot\buildTools.ps1"
 
 #Create output directory.
 #
-$binDirEpisode01 = Join-Path $PSScriptRoot "\..\bin\IMFaic.Probability\1.0.0"
+$binDir = Join-Path $PSScriptRoot "..\bin\IMFaic.Probability\1.0.1"
+$objDir = Join-Path $PSScriptRoot "..\obj\IMFaic.Probability\1.0.1"
 
-if (Test-Path $binDirEpisode01) {
-    rmdir -Recurse -Force $binDirEpisode01
+foreach ($directory in @($binDir, $objDir)) {
+    if (Test-Path $directory) {
+        rmdir -Recurse -Force $directory
+    }
+
+    mkdir $directory | out-null
 }
 
-mkdir $binDirEpisode01
 
 
-
-#Compile *.cs files.
-#Compile all *.cs files inside
+#Compile all *.cs files inside Probability\
 #
-#    Probability\
-#    IMFaic.Probability\src\
-#
-#and all of their subfolders.
+& $tools.csc `
+-nologo `
+-out:"$(Join-Path $binDir "Probability.dll")" `
+-recurse:"$(Join-Path $PSScriptRoot "..\..\Probability\*.cs")" `
+-target:"library" `
+$(&{
+    #Generate a temporary *.cs file that will make
+    #IMFaic.Probability a friend to Probability
+    #
+    $tmp = Join-Path $objDir "ProbabilityFriends.cs"
+    @(
+        "using System.Reflection;",
+        "using System.Runtime.CompilerServices;",
+        "",
+        "[assembly:InternalsVisibleTo(`"IMFaic.Probability`")]"
+    ) | Out-File -FilePath $tmp -Encoding utf8 -Force
+    $tmp
+})
+
+
+
+#Compile all *.cs files inside IMFaic.Probability\src\
 #
 & $tools.csc `
 -main:"IMFaic.Probability.Episode01" `
 -nologo `
--out:"$(Join-Path $binDirEpisode01 "IMFaic.Probability.exe")" `
--recurse:"$(Join-Path $PSScriptRoot "..\..\Probability\*.cs")" `
+-out:"$(Join-Path $binDir "IMFaic.Probability.exe")" `
 -recurse:"$(Join-Path $PSScriptRoot "..\src\*.cs")" `
+-reference:"$(Join-Path $binDir "Probability.dll")" `
 -target:"exe"
