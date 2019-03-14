2a373929a1bf683af265daced9fafe22ce62e2cd
Francis de la Cerna <imfrancisd@users.noreply.github.com>
2019-03-14 02:09:06 -0500

    Fix eng\buildtools.ps1.
    Fix verbose output.
    
    Store tools in hashtable on first use instead of the error prone use of
    multiple variables.

--- IMFaic.Probability/eng/buildTools.ps1
+++ IMFaic.Probability/eng/buildTools.ps1
@@ -9,6 +9,7 @@ param(
 
 $ErrorActionPreference = "Stop"
 $PSDefaultParameterValues = @{'Disabled' = $true}
+$tools = @{}
 
 
 
@@ -22,15 +23,15 @@ if (-not (Test-Path $OutputDirectory)) {
 
 Write-Verbose "Download nuget.exe (see https://www.nuget.org/downloads)."
 
-$nuget493 = Join-Path $OutputDirectory "nuget.exe"
+$tools.nuget = Join-Path $OutputDirectory "nuget.exe"
 
-if (-not (Test-Path $nuget493) -or (Get-FileHash $nuget493 -Algorithm SHA256).Hash -ne '1051f2053643a9fb5d8bf9ba7cf638164a3118541e90a671442cf3499c9606ef') {
+if (-not (Test-Path $tools.nuget) -or (Get-FileHash $tools.nuget -Algorithm SHA256).Hash -ne '1051f2053643a9fb5d8bf9ba7cf638164a3118541e90a671442cf3499c9606ef') {
     Invoke-WebRequest `
-    -OutFile $nuget493 `
+    -OutFile $tools.nuget `
     -Uri "https://dist.nuget.org/win-x86-commandline/v4.9.3/nuget.exe"
 }
 
-if (-not (Test-Path $nuget493) -or (Get-FileHash $nuget493 -Algorithm SHA256).Hash -ne '1051f2053643a9fb5d8bf9ba7cf638164a3118541e90a671442cf3499c9606ef') {
+if (-not (Test-Path $tools.nuget) -or (Get-FileHash $tools.nuget -Algorithm SHA256).Hash -ne '1051f2053643a9fb5d8bf9ba7cf638164a3118541e90a671442cf3499c9606ef') {
     throw "Could not download nuget.exe"
 }
 
@@ -38,33 +39,31 @@ if (-not (Test-Path $nuget493) -or (Get-FileHash $nuget493 -Algorithm SHA256).Ha
 
 Write-Verbose "Download compilers (see https://www.nuget.org/packages/Microsoft.Net.Compilers)"
 
-$csc = Join-Path $OutputDirectory "microsoft.net.compilers.2.10.0\tools\csc.exe"
-$csi = Join-Path $OutputDirectory "microsoft.net.compilers.2.10.0\tools\csi.exe"
-$vbc = Join-Path $OutputDirectory "microsoft.net.compilers.2.10.0\tools\vbc.exe"
+$tools.csc = Join-Path $OutputDirectory "microsoft.net.compilers.2.10.0\tools\csc.exe"
+$tools.csi = Join-Path $OutputDirectory "microsoft.net.compilers.2.10.0\tools\csi.exe"
+$tools.vbc = Join-Path $OutputDirectory "microsoft.net.compilers.2.10.0\tools\vbc.exe"
 
-if (-not ((Test-Path $csc) -and (Test-Path $csi) -and (Test-Path $vbc))) {
+if (-not ((Test-Path $tools.csc) -and (Test-Path $tools.csi) -and (Test-Path $tools.vbc))) {
+
     $(Join-Path $outputdirectory "microsoft.net.compilers.2.10.0") |
         Where-Object {Test-Path $_} |
         ForEach-Object {rmdir $_ -Recurse -Force}
-    & $nuget493 install `
+
+    & $tools.nuget install `
     "microsoft.net.compilers" `
     -OutputDirectory $OutputDirectory `
     -Source "https://api.nuget.org/v3/index.json" `
     -Verbosity "$(if ($VerbosePreference -eq "SilentlyContinue") {"quiet"} else {"normal"})" `
-    -Version "2.10.0"
+    -Version "2.10.0" |
+    ForEach-Object {Write-Verbose $_}
 }
 
-if (-not ((Test-Path $csc) -and (Test-Path $csi) -and (Test-Path $vbc))) {
+if (-not ((Test-Path $tools.csc) -and (Test-Path $tools.csi) -and (Test-Path $tools.vbc))) {
     throw "Could not get compilers from https://www.nuget.org/packages/Microsoft.Net.Compilers/2.10.0"
 }
 
 
 
-#Return tools in a hashtable containing their full path
-#
-@{
-    "nuget" = $nuget
-    "csc" = $csc
-    "csi" = $csi
-    "vbc" = $vbc
-}
+ #Return tools in a hashtable containing their full path
+ #
+ $tools
