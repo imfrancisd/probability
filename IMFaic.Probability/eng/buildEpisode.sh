b86a31002c0904cb45a2ce129fee388bc6df5aa5
Francis de la Cerna <imfrancisd@users.noreply.github.com>
2019-03-19 02:00:04 -0500

    Add buildEpisode.sh

---
+++ IMFaic.Probability/eng/buildEpisode.sh
@@ -0,0 +1,111 @@
+#!/usr/bin/env bash
+
+set -u
+
+SCRIPTROOT="$( dirname "${BASH_SOURCE[0]}" )"
+
+
+
+#Build all episodes if none are specified.
+#
+if [ "$#" -ne 0 ]
+then
+    IFS=" " read -ra EPISODE <<< "$@"
+else
+    IFS=" " read -ra EPISODE <<< "$(
+        for F in $( find "${SCRIPTROOT}/../src/Episodes/" -name "*.cs" )
+        do
+            F="$( basename "${F%.*}" )"
+            echo -n "${F/IMFaic\.Probability\.Episode/} "
+        done
+    )"
+fi
+
+
+
+csc -version &>/dev/null
+
+if [ $? -ne 0 ]
+then
+    echo "Requires C# compiler (csc)."
+    echo "Install Mono version 5.0.0 or higher."
+    exit 1
+fi    
+
+
+
+for EPISODENUMBER in "${EPISODE[@]}"
+do
+
+    echo "Build Episode${EPISODENUMBER}"
+
+    BINDIR="${SCRIPTROOT}/../bin/IMFaic.Probability/${EPISODENUMBER}.1.0"
+    OBJDIR="${SCRIPTROOT}/../obj/IMFaic.Probability/${EPISODENUMBER}.1.0"
+
+    rm -rf "${BINDIR}" "${OBJDIR}"
+    mkdir -p "${BINDIR}" "${OBJDIR}"
+
+
+
+    echo "Generating Source"
+
+    #Generate a file that will make IMFaic.Probability a friend to Probability
+    #
+    PROBABILITYFRIENDSCS="${OBJDIR}/ProbabilityFriends.cs"
+    echo "
+    using System.Reflection;
+    using System.Runtime.CompilerServices;
+
+    [assembly:InternalsVisibleTo(\"IMFaic.Probability\")]
+    " > "${PROBABILITYFRIENDSCS}"
+
+    #Generate a file that will run the episode in IMFaic.Probability
+    #
+    PROGRAMCS="${OBJDIR}/Program.cs"
+    echo "
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            IMFaic.Probability.Episode${EPISODENUMBER}.RunProbability();
+        }
+    }
+    " > "${PROGRAMCS}"
+
+
+
+    echo "Compiling Probability.dll"
+
+    csc \
+    -nologo \
+    -out:"${BINDIR}/Probability.dll" \
+    -recurse:"${SCRIPTROOT}/../../Probability/*.cs" \
+    -target:"library" \
+    "${PROBABILITYFRIENDSCS}"
+
+
+
+    echo "Compiling IMFaic.Probability.dll"
+
+    csc \
+    -nologo \
+    -out:"${BINDIR}/IMFaic.Probability.dll" \
+    -recurse:"${SCRIPTROOT}/../src/*.cs" \
+    -reference:"${BINDIR}/Probability.dll" \
+    -target:"library"
+
+
+
+    echo "Compiling IMFaic.Probability.exe"
+
+    csc \
+    -nologo \
+    -out:"${BINDIR}/Program.exe" \
+    -reference:"${BINDIR}/IMFaic.Probability.dll" \
+    -target:"exe" \
+    "${PROGRAMCS}"
+
+    mv "${BINDIR}/Program.exe" "${BINDIR}/IMFaic.Probability.exe"
+    
+done
+
