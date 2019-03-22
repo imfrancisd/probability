ecb6842aefff2cfc46c86ab1e17d86aa210db64d
Francis de la Cerna <imfrancisd@users.noreply.github.com>
2019-03-21 23:08:17 -0500

    Add IMFaic.Probability.Episode15.

---
+++ IMFaic.Probability/src/Episodes/IMFaic.Probability.Episode15.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Probability;
+using ProbabilityEp15 = Probability.Episode15;
+
+namespace IMFaic.Probability
+{
+    public static class Episode15
+    {
+        public static void RunProbability()
+        {
+            Console.WriteLine("Probability");
+            ProbabilityEp15.DoIt();
+            Console.WriteLine("Press Enter to finish");
+            Console.ReadLine();
+        }
+
+        public static void RunIMFaicProbability()
+        {
+            Console.WriteLine("IMFaic.Probability");
+            Console.WriteLine("Press Enter to finish");
+            Console.ReadLine();
+        }
+    }
+}
+
