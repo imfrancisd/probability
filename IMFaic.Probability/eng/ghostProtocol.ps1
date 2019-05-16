3b8db10c71fe3c434f2e6216fab466c75e320cb3
Francis de la Cerna <imfrancisd@users.noreply.github.com>
2019-05-15 20:24:07 -0500

    Update ghostProtocol.ps1.

--- IMFaic.Probability/eng/ghostProtocol.ps1
+++ IMFaic.Probability/eng/ghostProtocol.ps1
@@ -1,85 +1,130 @@
 #requires -version 5
 
 [cmdletbinding()]
-param()
+param(
+    [string]
+    $Name = "",
+
+    [string]
+    $Email = "",
+
+    [string]
+    $OriginUrl = "https://github.com/imfrancisd/probability.git",
+
+    [string]
+    $FaicUrl = "https://github.com/ericlippert/probability.git"
+)
+
+
 
 $ErrorActionPreference = "Stop"
 $PSDefaultParameterValues = @{"Disabled" = $true}
 
 
 
-git config user.name "Francis de la Cerna"
-git config user.email "imfrancisd@users.noreply.github.com"
+if (@(Get-Command "git" -ErrorAction SilentlyContinue).Count -eq 0) {
+    throw "Install git version control (see https://git-scm.com/)."
+}
+
+if ([string]::IsNullOrWhiteSpace((git config user.name))) {
+    if ([string]::IsNullOrWhiteSpace($Name)) {
+        throw "Configure git user.name (git config user.name `"Francis de la Cerna`")."
+    }
+    git config user.name $Name
+}
+
+if ([string]::IsNullOrWhiteSpace((git config user.email))) {
+    if ([string]::IsNullOrWhiteSpace($Email)) {
+        throw "Configure git user.email (git config user.email `"imfrancisd@users.noreply.github.com`")."
+    }
+    git config user.email $Email
+}
+
+if (@(git remote) -notcontains "origin") {
+    git remote add "origin" $OriginUrl
+}
 
 if (@(git remote) -notcontains "faic") {
-    git remote add "faic" "https://github.com/ericlippert/probability"
+    git remote add "faic" $FaicUrl
 }
+
+if ((git remote get-url "origin") -ne $OriginUrl) {
+    throw "git remote get-url `"origin`" is not `"$OriginUrl`"."
+}
+
+if ((git remote get-url "faic") -ne $FaicUrl) {
+    throw "git remote get-url `"faic`" is not `"$FaicUrl`"."
+}
+
+
 
 git fetch --all --quiet
 
-$faicBranches = @(git for-each-ref --format="%(refname:lstrip=3)" "refs/remotes/faic")
 $localBranches = @(git for-each-ref --format="%(refname:lstrip=2)" "refs/heads/")
 $originBranches = @(git for-each-ref --format="%(refname:lstrip=3)" "refs/remotes/origin")
-$diffBranches = @(
-    $faicBranches |
-        Where-Object {$originBranches -cnotcontains $_} |
-        Sort-Object {$_.Replace("episode", "") -as [double]}, {$_}
-)
+$faicBranches = @(git for-each-ref --format="%(refname:lstrip=3)" "refs/remotes/faic")
 
 
 
-foreach ($branch in $diffBranches) {
-    if ($localBranches -ccontains $branch) {
-        throw "New branch `"$branch`" in remotes/faic/ exists locally."
+Write-Verbose "Checking out `"ci/mission-impossible-ghost-protocol`" from `"origin/imfrancisd/mission-impossible-ghost-protocol`"."
+git checkout -B "ci/mission-impossible-ghost-protocol" "origin/imfrancisd/mission-impossible-ghost-protocol" --quiet
+
+foreach ($branch in $faicBranches) {
+    $episodeNumber = ($branch -replace "^episode", "") -as [double]
+
+    if ($null -eq $episodeNumber) {
+        Write-Verbose "Skipping branch `"$branch`" since it is not an episode."
+        continue
     }
 
-    #TODO: Push new branches to origin.
-    #TODO: Will assume branches not in origin have not yet been merged to origin.
+    $srcFile = Join-Path $PSScriptRoot "..\src\Episodes\IMFaic.Probability.Episode$($episodeNumber).cs"
 
-    git merge "faic/$branch" --quiet
+    if (-not (Test-Path $srcFile)) {
 
-    $episodeNumber = $branch.Replace("episode", "") -as [double]
+        #Assume that faic/$branch episode is not merged
+        #if IMFaic.Probability.Episode$($episodeNumber).cs does not exist.
 
-    if ($null -ne $episodeNumber) {
-        $srcFile = Join-Path $PSScriptRoot "..\src\Episodes\IMFaic.Probability.Episode$($episodeNumber).cs"
+        Write-Verbose "Merging `"faic/$branch`"."
+        git merge "faic/$branch" --quiet
 
-        if (-not (Test-Path $srcFile)) {
-            Write-Verbose "Creating $srcFile"
-            @(
-                "using System;"
-                "using Probability;"
-                "using ProbabilityEp$($episodeNumber) = Probability.Episode$($episodeNumber);"
-                ""
-                "namespace IMFaic.Probability"
-                "{"
-                "    public static class Episode$($episodeNumber)"
-                "    {"
-                "        public static void Run(string[] args)"
-                "        {"
-                "            RunProbability();"
-                "        }"
-                ""
-                "        public static void RunProbability()"
-                "        {"
-                "            Console.WriteLine(`"Probability`");"
-                "            ProbabilityEp$($episodeNumber).DoIt();"
-                "            Console.WriteLine(`"Press Enter to finish`");"
-                "            Console.ReadLine();"
-                "        }"
-                ""
-                "        public static void RunIMFaicProbability()"
-                "        {"
-                "            Console.WriteLine(`"IMFaic.Probability`");"
-                "            Console.WriteLine(`"Press Enter to finish`");"
-                "            Console.ReadLine();"
-                "        }"
-                "    }"
-                "}"
-            ) | Out-File -FilePath $srcFile -Encoding utf8 -Force
+        Write-Verbose "Creating `"$srcFile`"."
+        @(
+            "using System;"
+            "using Probability;"
+            "using ProbabilityEp$($episodeNumber) = Probability.Episode$($episodeNumber);"
+            ""
+            "namespace IMFaic.Probability"
+            "{"
+            "    public static class Episode$($episodeNumber)"
+            "    {"
+            "        public static void Run(string[] args)"
+            "        {"
+            "            RunProbability();"
+            "        }"
+            ""
+            "        public static void RunProbability()"
+            "        {"
+            "            Console.WriteLine(`"Probability`");"
+            "            ProbabilityEp$($episodeNumber).DoIt();"
+            "            Console.WriteLine(`"Press Enter to finish`");"
+            "            Console.ReadLine();"
+            "        }"
+            ""
+            "        public static void RunIMFaicProbability()"
+            "        {"
+            "            Console.WriteLine(`"IMFaic.Probability`");"
+            "            Console.WriteLine(`"Press Enter to finish`");"
+            "            Console.ReadLine();"
+            "        }"
+            "    }"
+            "}"
+        ) | Out-File -FilePath "$srcFile" -Encoding utf8 -Force
 
-            #TODO: Commit and push new file.
-        }
+        Write-Verbose "Commiting changes."
+        git add "$srcFile"
+        git commit -m "Add IMFaic.Probability.Episode$($episodeNumber)." --quiet
     }
 }
 
+#TODO: Push changes to origin?
 #TODO: Delete git config and secure environment variables?
