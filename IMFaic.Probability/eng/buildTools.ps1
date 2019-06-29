feb397049e9945e883b56f30b735fa90d2418b49
Francis de la Cerna <imfrancisd@users.noreply.github.com>
2019-06-29 18:02:51 -0500

    Update buildTools.ps1.

--- IMFaic.Probability/eng/buildTools.ps1
+++ IMFaic.Probability/eng/buildTools.ps1
@@ -14,53 +14,81 @@ param(
 
 $ErrorActionPreference = "Stop"
 $PSDefaultParameterValues = @{"Disabled" = $true}
-$tools = @{}
 
-
-
-#Create packages directory
-#
 if (-not (Test-Path $OutputDirectory)) {
     New-Item $OutputDirectory -ItemType "Directory" -Force | Out-Null
 }
 
+$tools = @{}
 
 
-Write-Verbose "Get compilers (see https://www.nuget.org/packages/microsoft.net.compilers.toolset)."
 
-$pkgName = "microsoft.net.compilers.toolset"
-$pkgVersion = "3.1.0"
-$pkgUri = "https://www.nuget.org/api/v2/package/$($pkgName)/$($pkgVersion)"
-$pkgDir = Join-Path $OutputDirectory "$($pkgName)/$($pkgVersion)"
-$pkgZip = Join-Path $OutputDirectory "$($pkgName).$($pkgVersion).nupkg.zip"
-$pkgZipSha512 = "80031aac4e6174a978135cc89030f59a914618e75053c48893087809311106747e4eb6921c62ae093e0c12603851a72a4e59277c7f3c956c314c7cfc7b66c762"
+function getNugetPackage
+{
+    [CmdletBinding()]
+    param(
+        [string]
+        $Name,
+        
+        [string]
+        $Version,
+        
+        [string]
+        $Sha512
+    )
 
-$tools.csc = Join-Path $pkgDir "tasks/$(if ($Framework -eq "Core") {"netcoreapp2.1/bincore"} else {"net472"})/csc.$(if ($Framework -eq "Core") {"dll"} else {"exe"})"
-$tools.vbc = Join-Path $pkgDir "tasks/$(if ($Framework -eq "Core") {"netcoreapp2.1/bincore"} else {"net472"})/vbc.$(if ($Framework -eq "Core") {"dll"} else {"exe"})"
+    Write-Verbose "Get package `"$($Name)`" version `"$($Version)`" from `"https://www.nuget.org/`"."
 
-if (-not ((Test-Path $tools.csc) -and (Test-Path $tools.vbc))) {
-    Invoke-WebRequest $pkgUri -OutFile $pkgZip
-    if ($pkgZipSha512 -ne (Get-FileHash $pkgZip -Algorithm SHA512).Hash) {
-        throw "Could not get compilers (nuget package $($pkgName).$($pkgVersion).nupkg.zip does not have expected sha512 sum)."
+    $pkgUri = "https://www.nuget.org/api/v2/package/$($Name)/$($Version)"
+    $pkgDir = Join-Path $OutputDirectory "$($Name)/$($Version)"
+    $pkgZip = Join-Path $OutputDirectory "$($Name).$($Version).nupkg.zip"
+
+    if (-not (Test-Path $pkgZip) -or ($Sha512 -ne (Get-FileHash $pkgZip -Algorithm "SHA512").Hash)) {
+        Invoke-WebRequest $pkgUri -OutFile $pkgZip
+    }
+
+    if ($Sha512 -ne (Get-FileHash $pkgZip -Algorithm "SHA512").Hash) {
+        throw "Could not get package `"$($Name)`" version `"$($Version)`" (downloaded file does not have the expected sha512 sum)."
     }
 
     if (Test-Path $pkgDir) {
         Remove-Item $pkgDir -Recurse -Force
     }
+
     New-Item $pkgDir -ItemType "Directory" -Force | Out-Null
     Expand-Archive $pkgZip -DestinationPath $pkgDir
 }
 
-if (-not ((Test-Path $tools.csc) -and (Test-Path $tools.vbc))) {
-    throw "Could not get compilers (nuget package $($pkgName).$($pkgVersion).nupkg.zip does not have expected folder structure)."
+
+
+Write-Verbose "Get Roslyn C# and VB compilers."
+getNugetPackage -Name "microsoft.net.compilers.toolset" -Version "3.1.0" -Sha512 "80031aac4e6174a978135cc89030f59a914618e75053c48893087809311106747e4eb6921c62ae093e0c12603851a72a4e59277c7f3c956c314c7cfc7b66c762"
+
+if ($Framework -eq "Core") {
+    $tools.roslyn = Join-Path $OutputDirectory "microsoft.net.compilers.toolset/3.1.0/tasks/netcoreapp2.1/bincore"
+    $tools.csc = [scriptblock]::Create("dotnet $(Join-Path $tools.roslyn "csc.dll") `$args")
+    $tools.vbc = [scriptblock]::Create("dotnet $(Join-Path $tools.roslyn "vbc.dll") `$args")
+}
+elseif ($Framework -eq "Mono") {
+    $tools.roslyn = Join-Path $OutputDirectory "microsoft.net.compilers.toolset/3.1.0/tasks/net472"
+    $tools.csc = [scriptblock]::Create("mono $(Join-Path $tools.roslyn "csc.exe") `$args")
+    $tools.csi = [scriptblock]::Create("mono $(Join-Path $tools.roslyn "csi.exe") `$args")
+    $tools.vbc = [scriptblock]::Create("mono $(Join-Path $tools.roslyn "vbc.exe") `$args")
+}
+else {
+    $tools.roslyn = Join-Path $OutputDirectory "microsoft.net.compilers.toolset/3.1.0/tasks/net472"
+    $tools.csc = [scriptblock]::Create("& $(Join-Path $tools.roslyn "csc.exe") `$args")
+    $tools.csi = [scriptblock]::Create("& $(Join-Path $tools.roslyn "csi.exe") `$args")
+    $tools.vbc = [scriptblock]::Create("& $(Join-Path $tools.roslyn "vbc.exe") `$args")
 }
 
 
 
-#Return tools in a hashtable containing script blocks that will invoke those tools.
-#
-$runner = if ($Framework -eq "Mono") {"mono"} elseif ($Framework -eq "Core") {"dotnet"} else {"&"}
-foreach ($key in @($tools.Keys)) {
-    $tools[$key] = [scriptblock]::Create("$($runner) $($tools[$key]) `$args")
-}
+Write-Verbose "Get .NET Standard reference assemblies."
+getNugetPackage -Name "netstandard.library" -Version "2.0.3" -Sha512 "e78f0cea69c14895b1b089644077dbce8631a626055d96522f4d29e061d8bfc3e48aa1419e74faf265b998612c03f721f5f0cef4690f824150a5689764dee601"
+
+$tools.netstandard = Join-Path $OutputDirectory "netstandard.library/2.0.3/build/netstandard2.0/ref"
+
+
+
 $tools
