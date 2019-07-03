7ee4105c871d83ae9abee337df4b2235936079b2
Francis de la Cerna <imfrancisd@users.noreply.github.com>
2019-07-02 21:44:52 -0500

    Update buildEpisode.sh.

--- IMFaic.Probability/eng/buildEpisode.sh
+++ IMFaic.Probability/eng/buildEpisode.sh
@@ -23,13 +23,6 @@ fi
 
 
 
-echo "Get build tools."
-
-PKGDIR="${SCRIPTROOT}/../packages"
-mkdir -p "${PKGDIR}"
-
-
-
 echo "Check if  mono is installed."
 
 mono --version 2>&1
@@ -42,34 +35,44 @@ fi
 
 
 
-echo "Get compilers (see https://www.nuget.org/packages/microsoft.net.compilers.toolset)."
+echo "Get build tools."
 
-PKGNAME="microsoft.net.compilers.toolset"
-PKGVERSION="3.1.0"
-PKGSHA512="80031aac4e6174a978135cc89030f59a914618e75053c48893087809311106747e4eb6921c62ae093e0c12603851a72a4e59277c7f3c956c314c7cfc7b66c762"
+function getNugetPackage {
+    echo "Get package \"${1}\" version \"${2}\" from \"https://www.nuget.org/\"."
 
-CSCEXE="${PKGDIR}/${PKGNAME}/${PKGVERSION}/tasks/net472/csc.exe"
+    PKGURI="https://www.nuget.org/api/v2/package/${1}/${2}"
+    PKGDIR="${SCRIPTROOT}/../packages/${1}/${2}"
+    PKGZIP="${SCRIPTROOT}/../packages/${1}.${2}.nupkg.zip"
 
-if [ ! -f "${CSCEXE}" ]
-then
-    rm -rf "${PKGDIR}/${PKGNAME}/${PKGVERSION}"
-    mkdir -p "${PKGDIR}/${PKGNAME}/${PKGVERSION}"
-    wget -O "${PKGDIR}/${PKGNAME}.${PKGVERSION}.nupkg.zip" "https://www.nuget.org/api/v2/package/Microsoft.Net.Compilers.Toolset/3.1.0" 2>&1
-
-    if [ "${PKGSHA512}" != "$(sha512sum "${PKGDIR}/${PKGNAME}.${PKGVERSION}.nupkg.zip" | cut -d " " -f 1)" ]
+    if [ ! -f ${PKGZIP} ] || [ "${3}" != "$(sha512sum "${PKGZIP}" | cut -d " " -f 1)" ]
     then
-        echo "Could not get compilers (nuget package ${PKGNAME}.${PKGVERSION}.nupkg.zip does not have expected SHA512 sum)."
+        wget -O "${PKGZIP}" "${PKGURI}" 2>&1
+    fi
+
+    if [ "${3}" != "$(sha512sum "${PKGZIP}" | cut -d " " -f 1)" ]
+    then
+        echo "Could not get package \"${1}\" version \"${2}\" (downloaded file does not have the expected sha512 sum)."
         exit 1
     fi
 
-    unzip "${PKGDIR}/${PKGNAME}.${PKGVERSION}.nupkg.zip" -d "${PKGDIR}/${PKGNAME}/${PKGVERSION}" 2>&1
-
-    if [ ! -f "${CSCEXE}" ]
+    if [ -f ${PKGDIR} ]
     then
-        echo "Could not get compilers (nuget package ${PKGNAME}.${PKGVERSION}.nupkg.zip does not have expected folder structure)."
-        exit 1
+        rm -rf ${PKGDIR}
     fi
-fi
+
+    mkdir -p ${PKGDIR}
+    unzip "${PKGZIP}" -d "${PKGDIR}" 2>&1
+}
+
+echo "Get Roslyn C# and VB compilers."
+
+getNugetPackage "microsoft.net.compilers.toolset" "3.1.0" "80031aac4e6174a978135cc89030f59a914618e75053c48893087809311106747e4eb6921c62ae093e0c12603851a72a4e59277c7f3c956c314c7cfc7b66c762"
+CSCEXE="${SCRIPTROOT}/../packages/microsoft.net.compilers.toolset/3.1.0/tasks/net472/csc.exe"
+
+echo "Get .NET Standard reference assemblies."
+
+getNugetPackage "netstandard.library" "2.0.3" "e78f0cea69c14895b1b089644077dbce8631a626055d96522f4d29e061d8bfc3e48aa1419e74faf265b998612c03f721f5f0cef4690f824150a5689764dee601"
+NETSTANDARD="${SCRIPTROOT}/../packages/netstandard.library/2.0.3/build/netstandard2.0/ref"
 
 
 
@@ -84,11 +87,14 @@ echo "Compiling Probability.dll"
 
 mono "${CSCEXE}" \
 -deterministic \
+-noconfig \
 -nologo \
+-nostdlib \
 -optimize \
 -out:"${LIBDIR}/Probability.dll" \
 -recurse:"${SCRIPTROOT}/../../Probability/*.cs" \
 -recurse:"${SCRIPTROOT}/../prb/*.cs" \
+-reference:"${NETSTANDARD}/netstandard.dll" \
 -target:"library"
 
 
@@ -97,11 +103,14 @@ echo "Compiling IMFaic.Probability.dll"
 
 mono "${CSCEXE}" \
 -deterministic \
+-noconfig \
 -nologo \
+-nostdlib \
 -optimize \
 -out:"${LIBDIR}/IMFaic.Probability.dll" \
 -recurse:"${SCRIPTROOT}/../src/*.cs" \
 -reference:"${LIBDIR}/Probability.dll" \
+-reference:"${NETSTANDARD}/netstandard.dll" \
 -target:"library"
 
 
@@ -136,10 +145,13 @@ do
 
     mono "${CSCEXE}" \
     -deterministic \
+    -noconfig \
     -nologo \
+    -nostdlib \
     -optimize \
     -out:"${BINDIR}/Program.exe" \
     -reference:"${BINDIR}/IMFaic.Probability.dll" \
+    -reference:"${NETSTANDARD}/netstandard.dll" \
     -target:"exe" \
     "${PROGRAMCS}"
 
